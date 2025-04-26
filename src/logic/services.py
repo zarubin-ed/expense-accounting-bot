52debe639946d15d2e205ad8437cbcdc7b8226c6
Егор Зарубин <egorzarubin@MacBook-Air-Egor.local>
2025-04-26 21:02:25 +0300

    add queries done

--- src/logic/services.py
+++ src/logic/services.py
@@ -3,21 +3,29 @@ from peewee import IntegrityError
 
 def add_user(user_id, username):
     """Добавляет нового пользователя"""
-    try:
-        user = user_ids.create(user_id=user_id, username=username)
-        return user
-    except IntegrityError:
-        print(f"Пользователь с ID {user_id} уже существует.")
-        return None
+    user = user_ids.create(username=username)
+    return user
 
 def add_group(group_id, chat_id):
     """Добавляет новую группу"""
-    group = group_ids.create(group_id=group_id, chat_id=chat_id)
+    group = group_ids.create(chat_id=chat_id)
     return group
 
-def add_group_members(member_id, chat_id, user_id):
-    group_member = group_members.create(member_id=member_id, chat_id=chat_id, user_id=user_id)
-    return group_ids
+def add_group_members(chat_id, user_id):
+    """Добавляет нового участника группы"""
+    group_member = group_members.create(chat_id=chat_id, user_id=user_id)
+    return group_member
+
+def add_depts(debtor_id, creditor_id, delta=0): 
+    """Добавляет пару людей, """
+    if debtor_id.group_id != creditor_id.group_id:
+        raise Exception("Кредитопр и должник должны быть в одной группе")
+    debt = debts.create(debtor_id=debtor_id, creditor_id=creditor_id, delta=delta)
+    return debt
+
+
+
+
 
 
 
